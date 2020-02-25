0ee0e66d34369cf94b553420b87c64d050910246
Harjinder Singh <38290866+Hellimax@users.noreply.github.com>
2020-02-25 19:31:15 +0530

    Update reinforcement_implementation.py

--- reinforcement_implementation.py
+++ reinforcement_implementation.py
@@ -1,27 +1,27 @@
 import numpy as np 
 
-R = np.matrix([[-1,-1,-1,-1,0,-1],
+R = np.matrix([[-1,-1,-1,-1,0,-1], #The reward matrix
                 [-1,-1,-1,0,-1,100],
                 [-1,-1,-1,0,-1,-1],
                 [-1,0,0,-1,0,-1],
                 [0,-1,-1,0,-1,100],
                 [-1,0,-1,-1,0,100]])
 
-Q = np.zeros((6,6))
-gamma = 0.8
+Q = np.zeros((6,6)) #the Q-matrix
+gamma = 0.8 #setting the gamma value
 
-def action_to_perform(state):
-    current_state_row = R[state,]
+def action_to_perform(state): #this is to find what steps can be taken by our agent to go to nect state  
+    current_state_row = R[state,]#for eg if the agent is at state 1 it can only go to state 3 and 5
     av_ac = np.where(current_state_row>=0)[1]
     action = np.random.choice(list(av_ac))
     return(action,av_ac)
 
-def update_q(current_state,action):
+def update_q(current_state,action): #this is to update Q-matrix
     _,action_list = action_to_perform(action)
     max_value = max(Q[action,action_list])
     Q[current_state,action] = R[current_state,action]+gamma*max_value
 
-def check_model():
+def check_model(): #this is to check our model after training
     current_state = int(input("Enter the current state "))
     path = [current_state]
     while current_state!=5:
@@ -36,7 +36,7 @@ def check_model():
     
 
 if __name__ == "__main__":
-    for i in range(10000):
+    for i in range(1000): #here we are training our model by 1000 iterations
         current_state = np.random.randint(0,5)
         action,_=action_to_perform(current_state)
         update_q(current_state,action)
